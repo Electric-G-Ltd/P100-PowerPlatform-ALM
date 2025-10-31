ecd5bf784764c380819720129e7a64f6f0152533
Gareth Youens <gareth@electricg.co.uk>
2025-10-31 09:45:24 +0000

    Changes to Lint Pull request

---
+++ GitHub_P100PPALM_Workspace/eslint.config.js
@@ -0,0 +1,25 @@
+// ESLint flat config for v9+
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+    js.configs.recommended,
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 2021,
+                sourceType: "module",
+            },
+        },
+        plugins: {
+            "@typescript-eslint": tseslint,
+        },
+        rules: {
+            semi: ["error", "always"],
+            quotes: ["error", "double"],
+        },
+    },
+];
